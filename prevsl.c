b05e2c0bc7cdc093cc7e6c5425ef250ed1d896b9
mh <observermatth@gmail.com>
2023-01-17 05:07:34 -0300

    stub(prevsl.c): stub on `prevsl` so that this branch can compile

--- prevsl.c
+++ prevsl.c
@@ -1,53 +1,107 @@
-#include "vslisp.h"
-#include "symtab.h"
+// prevsl: symbolic implementation of `libvsl';
+//         can be bootstrapped
 
-//static struct lisp_symtab_ret lisp_set(struct lisp_symtab_in in) {}
+//#include "libvsl.h" // <- TODO: stub
+//#include "prim.h"
 
-const struct lisp_symtab_raw lisp_symtab_prim[] = {
+#define CLISP(x) \
+  static int clisp_##x
+
+static struct clisp_symtab clisp_symtab[] = {
   // primitive functions
-  {"set",      0, __LISP_FUN, {lisp_set}},
-  {"func",     0, __LISP_FUN, {lisp_func}},
-  {"eval",     0, __LISP_FUN, {lisp_eval}},
-  {"quot",     0, __LISP_FUN, {lisp_quot}},
-  {"if",       0, __LISP_FUN, {lisp_if}},
-  {"whil",     0, __LISP_FUN, {lisp_while}},
-  {"break",    0, __LISP_FUN, {lisp_br}},
-  {"continue", 0, __LISP_FUN, {lisp_cont}},
+  {
+    .str  = "set", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_set,
+  },
+  {
+    .str = "func", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_func,
+  },
+  {
+    .str = "eval", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_eval,
+  },
+  {
+    .str = "quot", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_quot,
+  },
+  {
+    .str = "if", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_if,
+  },
+  {
+    .str = "while", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_while,
+  },
+  {
+    .str = "break", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_br,
+  },
+  {
+    .str = "continue", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_cont,
+  },
 
   // list-like functions
-  {"no-head",  0, __LISP_FUN, {lisp_nhead}},
-  {"head",     0, __LISP_FUN, {lisp_head}},
-  {"cat",      0, __LISP_FUN, {lisp_cat}},
+  {
+    .str = "no-head", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_nhead,
+  },
+  {
+    .str = "head", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_head,
+  },
+  {
+    .str = "cat", .hash = {.typ = __LISP_FUNC, .dat.func = NULL}, // = clisp_cat,
+  },
 
   // primitive symbols
-  {"t",        0, __LISP_SYM, {lisp_t}},
-  {"nil",      0, __LISP_SYM, {lisp_nil}},
+  {
+    .str = "t", .hash = {0}, // = clisp_t,
+  },
+  {
+    .str = "nil", .hash = {0}, // = clisp_nil,
+  },
 
   // EOL
-  {NULL,       0, {NULL}},
+  {0},
 };
 
-// TODO: make the symbol table integrate with `stack.h'
-// static
-
-static void compile_prim(void) {
-  struct lisp_symtab_raw* stab = NULL;
-  ulong hash = 0;
-
-  prim_symtab = symtab_new();
-
-  for (uint i = 0; ; ++i) {
-    stab = lisp_symtab_prim[i];
-
-    if (stab->str) {
-      hash = hash_str(stab->str);
-
-      symtab_add()
-      continue;
-    }
-
-    break;
-  }
+// TODO: this
+static int compile_grammar(void) {
+  return 0;
 }
 
-void (*frontend)(void) = compile_prim;
+static int compile_prim(void) {
+  int ret = 0;
+
+  struct clisp_symtab* stab;
+
+  for (uint i = 0;; ++i) {
+    stab = &clisp_symtab[i];
+
+    if (stab->str) {
+      // compile the hash for the table entry
+      assert_exec(
+        str_hash(stab), 1,
+        write(STDERR_FILENO,
+              MSG("[ !! ] prevsl: compiler error while parsing source\n")));
+
+      // add the entry to the symbol table
+      assert_exec(
+        lisp_symtab_add(stab->hash, stab->grammar), 1,
+        write(STDERR_FILENO,
+              MSG("[ !! ] prevsl: compiler error while parsing source\n"
+                  "  - could not enter item to the symbol table\n")));
+      }
+
+      continue;
+    }
+    else {
+      break;
+    }
+  }
+
+  done_for(ret);
+}
+
+static int prevsl(void) {
+  int ret = 0;
+
+  assert(compile_grammar() == 0, 1);
+  assert(compile_prim() == 0, 1);
+
+  done_for(ret);
+}
+
+VSL_FRONTEND(prevsl);
