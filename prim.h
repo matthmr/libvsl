858bfe0832a7ff4ceba3c5c9d1c54cea2f03da20
mh <observermatth@gmail.com>
2023-01-31 17:12:10 -0300

    stub(prim.c,prim.h): add to that `master` can compile
      - to be merged against other development branches

--- prim.h
+++ prim.h
@@ -1,6 +1,40 @@
 #ifndef LOCK_PRIM
 #  define LOCK_PRIM
 
+// the symbol table is defined by CGEN
+#  ifdef PROVIDE_PRIM_SYMTAB
+#    include "symtab.h"
+#  endif
 
+#  include "utils.h"
+
+struct lisp_fun_ret {
+  void* master; // TODO
+  int   slave;
+};
+
+struct lisp_fun_arg {
+  uint size[2];
+  uint litr[2];
+};
+
+struct lisp_fun_ret lisp_prim_set(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_fun(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_eval(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_quot(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_if(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_eq(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_not(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_block(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_while(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_break(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_continue(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_return(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_goto(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_label(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_cond(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_behead(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_head(struct lisp_fun_arg args);
+struct lisp_fun_ret lisp_prim_list(struct lisp_fun_arg args);
 
 #endif
