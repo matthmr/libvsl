03ca32d5893a35d5729283ff54ec30187b9d8903
mh <observermatth@gmail.com>
2023-01-31 17:12:10 -0300

    stub(prim.c,prim.h): add to that `master` can compile
      - to be merged against other development branches

---
+++ prim.c
@@ -0,0 +1,91 @@
+#include "prim.h"
+
+struct lisp_fun_ret lisp_prim_set(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_fun(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_eval(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_quot(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_if(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_eq(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_not(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_block(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_while(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_break(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_continue(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_return(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_goto(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_label(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_cond(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_behead(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_head(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
+
+struct lisp_fun_ret lisp_prim_list(struct lisp_fun_arg args) {
+  struct lisp_fun_ret ret = {0};
+  return ret;
+}
